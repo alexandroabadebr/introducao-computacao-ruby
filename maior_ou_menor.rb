3466fbdfe2f31e035c002d65db78563818a2a241
alexandroabadebr <alexandroabade@gmail.com>
2023-04-22 12:22:21 -0300

    Retorno de função

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -1,15 +1,24 @@
-puts "Bem-vindo ao jogo da adivinhação"
-puts "Qual é o seu nome?"
-nome = gets
-puts "\n\n\n\n\n\n"
-puts "Começaremos o jogo para você, " + nome
-puts "Escolhendo um número secreto entre 0 e 200..."
-numero_secreto = 175
-puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+def da_boas_vindas
+  puts "Bem-vindo ao jogo da adivinhação"
+  puts "Qual é o seu nome?"
+  nome = gets
+  puts "\n\n\n\n\n\n"
+  puts "Começaremos o jogo para você, " + nome
+end
+
+def sorteia_numero_secreto
+  puts "Escolhendo um número secreto entre 0 e 200..."
+  sorteado = 175
+  puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+  sorteado
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
 
 limite_de_tentativas = 5
 for tentativa in 1..limite_de_tentativas
-  puts "\n\n\n\n"
+  puts "\n\n"
   puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
   puts "Entre com o número"
   chute = gets
